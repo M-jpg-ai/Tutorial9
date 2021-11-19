7a7994e56aec979a74e2c0a9df9f57caf8d41e3c
Mohnish <78406326+M-jpg-ai@users.noreply.github.com>
2021-11-19 14:48:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+var = "hello"
+print(var)
+
+num=123
+num_class_1= type(num)
+num1=str(num)
+num_class=type(num1)
+
+print(num_class)
+print(num_class_1)
+
+
+num3="445"
+num4=int(num3)
+print(num4)
+typeofdata=type(num4)
+print(typeofdata)
+
+dict1={"mohnish":"coding","pyhton":"java"}
+dict2=dict1["mohnish"]
+print(dict2)
